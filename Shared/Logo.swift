0fad18722333cf255cf1ed18c068a5d2173b231c
Kamaal Farah <kamaal.f1@gmail.com>
2020-06-25 22:23:48 +0200

    Initializing logo

---
+++ Shared/Logo.swift
@@ -0,0 +1,21 @@
+//
+//  Logo.swift
+//  SuperSmashBrosStats
+//
+//  Created by Kamaal Farah on 25/06/2020.
+//  Copyright © 2020 Kamaal. All rights reserved.
+//
+
+import SwiftUI
+
+struct Logo: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct Logo_Previews: PreviewProvider {
+    static var previews: some View {
+        Logo()
+    }
+}
